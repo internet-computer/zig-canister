2967ed327a0637bf579a77a673b08d24c001f871
EnzoPlayer0ne <enzo@desiage.org>
2024-06-18 10:54:46 +0200

    now it builds

--- organic/qr-code/build.zig
+++ organic/qr-code/build.zig
@@ -1,8 +1,30 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    var wasm = b.addExecutable(.{
-        .name = "main",
+    const qrcodegen = b.addStaticLibrary(.{
+        .name = "qrcodegen",
+        .target = b.resolveTargetQuery(.{
+            .cpu_arch = .wasm32,
+            .os_tag = .freestanding,
+        }),
+        .optimize = .ReleaseSmall,
+    });
+    qrcodegen.addCSourceFile(.{ .file = .{ .path = "Qr-Code-generator/c/qrcodegen.c" }, .flags = &[_][]const u8{} });
+    qrcodegen.addIncludePath(.{ .path = "Qr-Code-generator/c/" });
+
+    const libc = b.addStaticLibrary(.{
+        .name = "libc",
+        .target = b.resolveTargetQuery(.{
+            .cpu_arch = .wasm32,
+            .os_tag = .freestanding,
+        }),
+        .optimize = .ReleaseSmall,
+    });
+    libc.addCSourceFile(.{ .file = .{ .path = "Qr-Code-generator/c/libc.c" }, .flags = &[_][]const u8{} });
+    libc.addIncludePath(.{ .path = "Qr-Code-generator/c/" });
+
+    const qr = b.addExecutable(.{
+        .name = "qr",
         .root_source_file = b.path("src/main.zig"),
         .target = b.resolveTargetQuery(.{
             .cpu_arch = .wasm32,
@@ -11,16 +33,29 @@ pub fn build(b: *std.Build) void {
         .optimize = .ReleaseSmall,
         .strip = true,
     });
-
-    wasm.entry = .disabled;
-    wasm.root_module.export_symbol_names = &[_][]const u8{
+    qr.addIncludePath(.{ .path = "Qr-Code-generator/c/" });
+    qr.entry = .disabled;
+    qr.root_module.export_symbol_names = &[_][]const u8{
         "canister_update go",
     };
 
-    wasm.addObjectFile(.{ .path = "./Qr-Code-generator/zig-out/lib/libqrcodegen.a" });
-    wasm.addObjectFile(.{ .path = "./Qr-Code-generator/zig-out/lib/liblibc.a" });
+    b.installArtifact(qr);
+    b.installArtifact(qrcodegen);
+    b.installArtifact(libc);
 
-    wasm.addIncludePath(.{ .path = "Qr-Code-generator/c" });
+    // wasm.addObjectFile(.{ .path = qr.out_lib_filename });
+    // wasm.addObjectFile(.{ .path = qrcodegen.out_lib_filename });
+    // wasm.addObjectFile(.{ .path = libc.out_lib_filename });
 
-    b.installArtifact(wasm);
+    //  wasm = b.addExecutable(.{
+    //     .name = "qr_wasm",
+    //     .target = b.resolveTargetQuery(.{
+    //         .cpu_arch = .wasm32,
+    //         .os_tag = .freestanding,
+    //     }),
+    //     .optimize = .ReleaseSmall,
+    //     .strip = true,
+    // });
+
+    // // b.installArtifact(wasm);
 }
