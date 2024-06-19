9adaec8cefd91045c54ac93c3d98d320625b52e3
EnzoPlayer0ne <enzo@desiage.org>
2024-06-19 10:29:21 +0200

    save

---
+++ organic/qr-code/Qr-Code-generator/c/stdlib.h
@@ -0,0 +1,2 @@
+int abs(int);
+long labs(long);
---
+++ organic/qr-code/Qr-Code-generator/c/string.h
@@ -0,0 +1,8 @@
+#include <stddef.h>
+size_t strlen(const char*);
+void *memset(void *, int, size_t);
+void *memchr(const void *, int, size_t);
+char *strchr(const char *, int);
+void* memmove(void *, const void *, size_t);
+void* memcpy(void *, const void *, size_t);
+int memcmp(const void *, const void *, size_t);
